16d35f38d6cf9bf6328ff7bb5baca07c00a20ee4
Vladislav Antonov <lolik1806@gmail.com>
2023-02-16 11:32:12 +0300

    init ui

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,11 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+
+        this.WidthRequest = 400;
+        this.HeightRequest = 600;
+    }
 }
 
